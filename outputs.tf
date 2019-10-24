c6022401b8cc7e64ada2c7c276fa2d0c42fbfa41
Michael Voigt <michael.voigt@wlw.de>
2019-10-24 11:00:49 +0200

    Update to terraform 0.12.x -> run terraform 0.12upgrade

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,50 @@
 output "master_username" {
-  value       = "${join("", aws_docdb_cluster.default.*.master_username)}"
+  value       = join("", aws_docdb_cluster.default.*.master_username)
   description = "Username for the master DB user"
 }
 
 output "cluster_name" {
-  value       = "${join("", aws_docdb_cluster.default.*.cluster_identifier)}"
+  value       = join("", aws_docdb_cluster.default.*.cluster_identifier)
   description = "Cluster Identifier"
 }
 
 output "arn" {
-  value       = "${join("", aws_docdb_cluster.default.*.arn)}"
+  value       = join("", aws_docdb_cluster.default.*.arn)
   description = "Amazon Resource Name (ARN) of the cluster"
 }
 
 output "endpoint" {
-  value       = "${join("", aws_docdb_cluster.default.*.endpoint)}"
+  value       = join("", aws_docdb_cluster.default.*.endpoint)
   description = "Endpoint of the DocumentDB cluster"
 }
 
 output "reader_endpoint" {
-  value       = "${join("", aws_docdb_cluster.default.*.reader_endpoint)}"
+  value       = join("", aws_docdb_cluster.default.*.reader_endpoint)
   description = "A read-only endpoint of the DocumentDB cluster, automatically load-balanced across replicas"
 }
 
 output "master_host" {
-  value       = "${module.dns_master.hostname}"
+  value       = module.dns_master.hostname
   description = "DB master hostname"
 }
 
 output "replicas_host" {
-  value       = "${module.dns_replicas.hostname}"
+  value       = module.dns_replicas.hostname
   description = "DB replicas hostname"
 }
 
 output "security_group_id" {
   description = "ID of the DocumentDB cluster Security Group"
-  value       = "${join("", aws_security_group.default.*.id)}"
+  value       = join("", aws_security_group.default.*.id)
 }
 
 output "security_group_arn" {
   description = "ARN of the DocumentDB cluster Security Group"
-  value       = "${join("", aws_security_group.default.*.arn)}"
+  value       = join("", aws_security_group.default.*.arn)
 }
 
 output "security_group_name" {
   description = "Name of the DocumentDB cluster Security Group"
-  value       = "${join("", aws_security_group.default.*.name)}"
+  value       = join("", aws_security_group.default.*.name)
 }
+
--- variables.tf
+++ variables.tf
@@ -4,176 +4,177 @@ variable "enabled" {
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `eg` or `cp`)"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name of the application"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `name`, `namespace`, `stage` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "zone_id" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Route53 parent zone ID. If provided (not empty), the module will create sub-domain DNS records for the DocumentDB master and replicas"
 }
 
 variable "allowed_security_groups" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of existing Security Groups to be allowed to connect to the DocumentDB cluster"
 }
 
 variable "allowed_cidr_blocks" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of CIDR blocks to be allowed to connect to the DocumentDB cluster"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "VPC ID to create the cluster in (e.g. `vpc-a22222ee`)"
 }
 
 variable "subnet_ids" {
-  type        = "list"
+  type        = list(string)
   description = "List of VPC subnet IDs to place DocumentDB instances"
 }
 
 variable "instance_class" {
-  type        = "string"
+  type        = string
   default     = "db.r4.large"
   description = "The instance class to use. For more details, see https://docs.aws.amazon.com/documentdb/latest/developerguide/db-instance-classes.html#db-instance-class-specs"
 }
 
 variable "cluster_size" {
-  type        = "string"
+  type        = string
   default     = "3"
   description = "Number of DB instances to create in the cluster"
 }
 
 variable "snapshot_identifier" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "Specifies whether or not to create this cluster from a snapshot. You can use either the name or ARN when specifying a DB cluster snapshot, or the ARN when specifying a DB snapshot"
 }
 
 variable "db_port" {
-  type        = "string"
+  type        = string
   default     = "27017"
   description = "DocumentDB port"
 }
 
 variable "master_username" {
-  type        = "string"
+  type        = string
   default     = "admin1"
   description = "(Required unless a snapshot_identifier is provided) Username for the master DB user"
 }
 
 variable "master_password" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "(Required unless a snapshot_identifier is provided) Password for the master DB user. Note that this may show up in logs, and it will be stored in the state file. Please refer to the DocumentDB Naming Constraints"
 }
 
 variable "retention_period" {
-  type        = "string"
+  type        = string
   default     = "5"
   description = "Number of days to retain backups for"
 }
 
 variable "preferred_backup_window" {
-  type        = "string"
+  type        = string
   default     = "07:00-09:00"
   description = "Daily time range during which the backups happen"
 }
 
 variable "cluster_parameters" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of DB parameters to apply"
 }
 
 variable "cluster_family" {
-  type        = "string"
+  type        = string
   default     = "docdb3.6"
   description = "The family of the DocumentDB cluster parameter group. For more details, see https://docs.aws.amazon.com/documentdb/latest/developerguide/db-cluster-parameter-group-create.html"
 }
 
 variable "engine" {
-  type        = "string"
+  type        = string
   default     = "docdb"
   description = "The name of the database engine to be used for this DB cluster. Defaults to `docdb`. Valid values: `docdb`"
 }
 
 variable "engine_version" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "The version number of the database engine to use"
 }
 
 variable "storage_encrypted" {
-  type        = "string"
+  type        = string
   description = "Specifies whether the DB cluster is encrypted"
   default     = "true"
 }
 
 variable "kms_key_id" {
-  type        = "string"
+  type        = string
   description = "The ARN for the KMS encryption key. When specifying `kms_key_id`, `storage_encrypted` needs to be set to `true`"
   default     = ""
 }
 
 variable "skip_final_snapshot" {
-  type        = "string"
+  type        = string
   description = "Determines whether a final DB snapshot is created before the DB cluster is deleted"
   default     = "true"
 }
 
 variable "apply_immediately" {
-  type        = "string"
+  type        = string
   description = "Specifies whether any cluster modifications are applied immediately, or during the next maintenance window"
   default     = "true"
 }
 
 variable "enabled_cloudwatch_logs_exports" {
-  type        = "list"
+  type        = list(string)
   description = "List of log types to export to cloudwatch. The following log types are supported: audit, error, general, slowquery."
   default     = []
 }
 
 variable "cluster_dns_name" {
-  type        = "string"
+  type        = string
   description = "Name of the cluster CNAME record to create in the parent DNS zone specified by `zone_id`. If left empty, the name will be auto-asigned using the format `master.var.name`"
   default     = ""
 }
 
 variable "reader_dns_name" {
-  type        = "string"
+  type        = string
   description = "Name of the reader endpoint CNAME record to create in the parent DNS zone specified by `zone_id`. If left empty, the name will be auto-asigned using the format `replicas.var.name`"
   default     = ""
 }
+
