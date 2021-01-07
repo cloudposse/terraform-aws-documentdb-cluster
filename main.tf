b74c92a5851cc04613972a521d7d1585bedfc63d
Maxim Mironenko <maxim@cloudposse.com>
2021-01-07 15:18:17 +0700

    Terraform 0.14 upgrade (#20)
    * Terraform 0.14 upgrade
    
    * revert local Makefile fix

--- main.tf
+++ main.tf
@@ -1,24 +1,13 @@
-module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
-  enabled    = var.enabled
-  namespace  = var.namespace
-  name       = var.name
-  stage      = var.stage
-  delimiter  = var.delimiter
-  attributes = var.attributes
-  tags       = var.tags
-}
-
 resource "aws_security_group" "default" {
-  count       = var.enabled ? 1 : 0
-  name        = module.label.id
+  count       = module.this.enabled ? 1 : 0
+  name        = module.this.id
   description = "Security Group for DocumentDB cluster"
   vpc_id      = var.vpc_id
-  tags        = module.label.tags
+  tags        = module.this.tags
 }
 
 resource "aws_security_group_rule" "egress" {
-  count             = var.enabled ? 1 : 0
+  count             = module.this.enabled ? 1 : 0
   type              = "egress"
   description       = "Allow all egress traffic"
   from_port         = 0
@@ -29,7 +18,7 @@ resource "aws_security_group_rule" "egress" {
 }
 
 resource "aws_security_group_rule" "ingress_security_groups" {
-  count                    = var.enabled ? length(var.allowed_security_groups) : 0
+  count                    = module.this.enabled ? length(var.allowed_security_groups) : 0
   type                     = "ingress"
   description              = "Allow inbound traffic from existing Security Groups"
   from_port                = var.db_port
@@ -41,7 +30,7 @@ resource "aws_security_group_rule" "ingress_security_groups" {
 
 resource "aws_security_group_rule" "ingress_cidr_blocks" {
   type              = "ingress"
-  count             = var.enabled && length(var.allowed_cidr_blocks) > 0 ? 1 : 0
+  count             = module.this.enabled && length(var.allowed_cidr_blocks) > 0 ? 1 : 0
   description       = "Allow inbound traffic from CIDR blocks"
   from_port         = var.db_port
   to_port           = var.db_port
@@ -51,14 +40,14 @@ resource "aws_security_group_rule" "ingress_cidr_blocks" {
 }
 
 resource "aws_docdb_cluster" "default" {
-  count                           = var.enabled ? 1 : 0
-  cluster_identifier              = module.label.id
+  count                           = module.this.enabled ? 1 : 0
+  cluster_identifier              = module.this.id
   master_username                 = var.master_username
   master_password                 = var.master_password
   backup_retention_period         = var.retention_period
   preferred_backup_window         = var.preferred_backup_window
   preferred_maintenance_window    = var.preferred_maintenance_window
-  final_snapshot_identifier       = lower(module.label.id)
+  final_snapshot_identifier       = lower(module.this.id)
   skip_final_snapshot             = var.skip_final_snapshot
   apply_immediately               = var.apply_immediately
   storage_encrypted               = var.storage_encrypted
@@ -71,32 +60,32 @@ resource "aws_docdb_cluster" "default" {
   engine                          = var.engine
   engine_version                  = var.engine_version
   enabled_cloudwatch_logs_exports = var.enabled_cloudwatch_logs_exports
-  tags                            = module.label.tags
+  tags                            = module.this.tags
 }
 
 resource "aws_docdb_cluster_instance" "default" {
-  count                      = var.enabled ? var.cluster_size : 0
-  identifier                 = "${module.label.id}-${count.index + 1}"
+  count                      = module.this.enabled ? var.cluster_size : 0
+  identifier                 = "${module.this.id}-${count.index + 1}"
   cluster_identifier         = join("", aws_docdb_cluster.default.*.id)
   apply_immediately          = var.apply_immediately
   instance_class             = var.instance_class
-  tags                       = module.label.tags
+  tags                       = module.this.tags
   engine                     = var.engine
   auto_minor_version_upgrade = var.auto_minor_version_upgrade
 }
 
 resource "aws_docdb_subnet_group" "default" {
-  count       = var.enabled ? 1 : 0
-  name        = module.label.id
+  count       = module.this.enabled ? 1 : 0
+  name        = module.this.id
   description = "Allowed subnets for DB cluster instances"
   subnet_ids  = var.subnet_ids
-  tags        = module.label.tags
+  tags        = module.this.tags
 }
 
 # https://docs.aws.amazon.com/documentdb/latest/developerguide/db-cluster-parameter-group-create.html
 resource "aws_docdb_cluster_parameter_group" "default" {
-  count       = var.enabled ? 1 : 0
-  name        = module.label.id
+  count       = module.this.enabled ? 1 : 0
+  name        = module.this.id
   description = "DB cluster parameter group"
   family      = var.cluster_family
 
@@ -109,28 +98,34 @@ resource "aws_docdb_cluster_parameter_group" "default" {
     }
   }
 
-  tags = module.label.tags
+  tags = module.this.tags
 }
 
 locals {
-  cluster_dns_name_default  = "master.${var.name}"
+  cluster_dns_name_default  = "master.${module.this.name}"
   cluster_dns_name          = var.cluster_dns_name != "" ? var.cluster_dns_name : local.cluster_dns_name_default
-  replicas_dns_name_default = "replicas.${var.name}"
+  replicas_dns_name_default = "replicas.${module.this.name}"
   replicas_dns_name         = var.reader_dns_name != "" ? var.reader_dns_name : local.replicas_dns_name_default
 }
 
 module "dns_master" {
-  source  = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.7.0"
-  enabled = var.enabled && var.zone_id != "" ? true : false
+  source  = "cloudposse/route53-cluster-hostname/aws"
+  version = "0.8.0"
+  enabled = module.this.enabled && var.zone_id != "" ? true : false
   name    = local.cluster_dns_name
   zone_id = var.zone_id
   records = coalescelist(aws_docdb_cluster.default.*.endpoint, [""])
+
+  context = module.this.context
 }
 
 module "dns_replicas" {
-  source  = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.7.0"
-  enabled = var.enabled && var.zone_id != "" ? true : false
+  source  = "cloudposse/route53-cluster-hostname/aws"
+  version = "0.8.0"
+  enabled = module.this.enabled && var.zone_id != "" ? true : false
   name    = local.replicas_dns_name
   zone_id = var.zone_id
   records = coalescelist(aws_docdb_cluster.default.*.reader_endpoint, [""])
+
+  context = module.this.context
 }
