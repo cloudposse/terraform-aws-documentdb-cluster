e59623b849f63c448013071e9c27ddb1e527751d
Tom Di Nunzio <tom.dinunzio@trayport.com>
2025-08-14 10:51:34 +0100

    Support for serverless clusters

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.29.0"
+      version = ">= 6.8.0"
     }
     local = {
       source  = "hashicorp/local"
