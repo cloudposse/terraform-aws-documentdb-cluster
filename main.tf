3b25ed59e5a415acb369cd29d391b1c575b7abf2
Dimitrij Kimel <dimitrij.kimel@sva.de>
2025-11-25 09:58:23 +0100

    fix: Unique versioned paramter_group name

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "aws_docdb_subnet_group" "default" {
 # https://docs.aws.amazon.com/documentdb/latest/developerguide/db-cluster-parameter-group-create.html
 resource "aws_docdb_cluster_parameter_group" "default" {
   count       = local.enabled ? 1 : 0
-  name        = module.this.id
+  name        = "${module.this.id}-v-${replace(var.cluster_family, ".", "-")}"
   description = "DB cluster parameter group"
   family      = var.cluster_family
 
